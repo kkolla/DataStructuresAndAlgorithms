0b1dacdd5c076ee20e67248ad719f10b160d9f2d
kartheek kolla <kolla@outlook.com>
2016-01-13 22:19:08 -0800

    Regex Matching - more tests

--- randomProgrammingPuzzles/puzzles.h
+++ randomProgrammingPuzzles/puzzles.h
@@ -1,10 +1,4 @@
 #include <iostream>
 #include <math.h>
 #include <string.h>
-#define LEVELS 4
 using namespace std;
-void regexUsage()
-{
-    printf("regexMatch <String> <Regex>\n");
-    exit(1);
-}
